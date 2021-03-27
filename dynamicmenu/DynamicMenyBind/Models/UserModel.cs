cc23ecb3b014e3fe0dfe2508cfb284118ee46866
shabahat97 <shabahatalichishti@gmail.com>
2021-03-27 16:59:33 +0500

    shabahat

---
+++ dynamicmenu/DynamicMenyBind/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicMenyBind.Models
+{
+    public class UserModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email_Address { get; set; }
+        public string Phonenumber { get; set; }
+
+        public string Date { get; set; }
+
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+
+
+
+    }
+}
